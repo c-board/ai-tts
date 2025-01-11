00bf8f21963b1210d277dc90e44d866ada3ef3a3
Colin Boardway <cmboardway@gmail.com>
2025-01-11 07:30:56 -0600

    remove popup files

--- scripts/popup.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("test").addEventListener("click", () => {
-  const speech = new SpeechSynthesisUtterance(
-    "This is a test of the text-to-speech functionality."
-  );
-  window.speechSynthesis.speak(speech);
-});
